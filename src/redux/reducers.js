891e7e5169a3251a0376d6e83cae11abee43b91f
hellohyonee <hellohk07@gmail.com>
2022-05-18 02:25:17 +0900

    action함수 내보내기, reducers 생성

---
+++ src/redux/reducers.js
@@ -0,0 +1,16 @@
+import { ADD_TODO } from "./actions"
+
+const initialState = []; // 초기값을 먼저 설정하여 자동으로 previousState가 초기값을 알게 함
+
+function todoApp(previousState = initialState, action) {
+  // 초기값 설정
+  // if (previousState === undefined) {
+  //   return [];
+  // }
+
+  if (action.type === ADD_TODO) {
+    return [...previousState, action.todo];  // immutable하게 처리 -> 리듀서를 통해 스테이트 감지
+  }
+
+  return previousState; // 해당 action함수가 안 들어오면 이전 상태 그대로
+}
