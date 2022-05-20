4b2bc3cffec5a0fbd88cdeb54c358d9118d74e28
hellohyonee <hellohk07@gmail.com>
2022-05-20 18:02:42 +0900

    todos 상태에 필터를 적용하기 위해 객체에 넣기 {todos:[{},{},...], filter:}

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,10 +1,11 @@
 import { ADD_TODO, COMPLETE_TODO } from "./actions"
 
-const initialState = []; // 초기값을 먼저 설정하여 자동으로 previousState가 초기값을 알게 함
-
-// state
+// state -> todo
 // -> ['코딩', '점심먹기']
-// --> [{text: '코딩', done: false}, {text: '점심먹기', done: true}]
+// --> [{text: '코딩', done: false}, {text: '점심먹기', done: false}]
+// ---> {todos: [{text: '코딩', done: false}, {text: '점심먹기', done: false}], filter: 'ALL'}
+const initialState = {todos: [], filter: 'ALL'}; // 초기값을 먼저 설정하여 자동으로 previousState가 초기값을 알게 함
+
 export function todoApp(previousState = initialState, action) {
   // 초기값 설정
   // if (previousState === undefined) {
@@ -12,16 +13,22 @@ export function todoApp(previousState = initialState, action) {
   // }
 
   if (action.type === ADD_TODO) {
-    return [...previousState, { text: action.text, done: false }];  // immutable하게 처리 -> 리듀서를 통해 스테이트 감지
+    return {
+      ...previousState,
+      todos: [...previousState.todos, { text: action.text, done: false }]
+    };  // immutable하게 처리 -> 리듀서를 통해 스테이트 감지
   }
 
   if (action.type === COMPLETE_TODO) {
-    return previousState.map((todo, index) => {
+    return {
+      ...previousState,
+      todos: previousState.todos.map((todo, index) => {
       if (index === action.index) {
-        return { ...todo, done: true };
+        return { ...todo, done: true }; 
       }
       return todo;
-    })
+      })
+    };
   }
 
   return previousState; // 해당 action함수가 안 들어오면 이전 상태 그대로
