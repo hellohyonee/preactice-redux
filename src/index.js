73cf759dad8acd3f6d0da3385d6783c768694aa0
hellohyonee <hellohk07@gmail.com>
2022-05-20 17:41:08 +0900

    completeTodo 액션, 리듀서 추가

--- src/index.js
+++ src/index.js
@@ -3,23 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import store from './redux/store';
-import { addTodo } from './redux/actions';
+import { addTodo, completeTodo } from './redux/actions';
 
-const unsubscribe = store.subscribe(() => {   // subscribe: store 변경사항을 구독 -> return 결과는 구독함수 제거
+store.subscribe(() => {   // subscribe: store 변경사항을 구독 -> return 결과는 구독함수 제거
   console.log(store.getState());  // getState: 현재 store의 state를 가져옴
 }); 
 
 // console.log(store);
-
-store.dispatch(addTodo('coding'));  // dispatch: action함수를 넣어서 상태 변경
-store.dispatch(addTodo('reading book'));
-store.dispatch(addTodo('eating'));
-unsubscribe();  // 구독 제거(아래의 값이 나오지는 않음).
-// * 아래의 코드는 구독을 제거했지만 스테이트에 쌓이면서 실제 store에는 변경이 진행됨
-store.dispatch(addTodo('coding'));
-store.dispatch(addTodo('reading book'));
-store.dispatch(addTodo('eating'));
-
+store.dispatch(addTodo('점심먹기'));
+store.dispatch(completeTodo(0));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,10 @@
-import { ADD_TODO } from "./actions"
+import { ADD_TODO, COMPLETE_TODO } from "./actions"
 
 const initialState = []; // 초기값을 먼저 설정하여 자동으로 previousState가 초기값을 알게 함
 
+// state
+// -> ['코딩', '점심먹기']
+// --> [{text: '코딩', done: false}, {text: '점심먹기', done: true}]
 export function todoApp(previousState = initialState, action) {
   // 초기값 설정
   // if (previousState === undefined) {
@@ -9,7 +12,16 @@ export function todoApp(previousState = initialState, action) {
   // }
 
   if (action.type === ADD_TODO) {
-    return [...previousState, action.todo];  // immutable하게 처리 -> 리듀서를 통해 스테이트 감지
+    return [...previousState, { text: action.text, done: false }];  // immutable하게 처리 -> 리듀서를 통해 스테이트 감지
+  }
+
+  if (action.type === COMPLETE_TODO) {
+    return previousState.map((todo, index) => {
+      if (index === action.index) {
+        return { ...todo, done: true };
+      }
+      return todo;
+    })
   }
 
   return previousState; // 해당 action함수가 안 들어오면 이전 상태 그대로
