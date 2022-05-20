47e22f593176cb6f12a122e277dee2e4cc1d7cca
hellohyonee <hellohk07@gmail.com>
2022-05-20 19:28:44 +0900

    redux 없이 react로 연결하기(hooks 사용)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import { addTodo } from './redux/actions';
 
-function App() {
+function App({ store }) {
+  const [state, setState] =useState(store.getState());
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setState(store.getState());
+    })
+    return () => {
+      unsubscribe();
+    }
+  }, [store])
+
   return (
     <>
-    Hello World!!
+      {JSON.stringify(state)}
+      <button onClick={click}>추가</button>
     </>
   );
+
+  function click() {
+    store.dispatch(addTodo('todo'));
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import store from './redux/store';
-import { addTodo, completeTodo, ShowComplete } from './redux/actions';
+// import { addTodo, completeTodo, ShowComplete } from './redux/actions';
 
-store.subscribe(() => {   // subscribe: store 변경사항을 구독 -> return 결과는 구독함수 제거
-  console.log(store.getState());  // getState: 현재 store의 state를 가져옴
-}); 
+// store.subscribe(() => {   // subscribe: store 변경사항을 구독 -> return 결과는 구독함수 제거
+//   console.log(store.getState());  // getState: 현재 store의 state를 가져옴
+// }); 
 
-// console.log(store);
-store.dispatch(addTodo('점심먹기'));
-store.dispatch(completeTodo(0));
-store.dispatch(ShowComplete());
+// // console.log(store);
+// store.dispatch(addTodo('점심먹기'));
+// store.dispatch(completeTodo(0));
+// store.dispatch(ShowComplete());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>
-);
+); 
