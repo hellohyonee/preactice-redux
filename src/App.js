d2e1eb0d57ef6b7ee100b962e1eda6327407cea2
hellohyonee <hellohk07@gmail.com>
2022-05-21 23:31:36 +0900

    Custom Hook으로 store에서 변경된 state 받아오기

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import { useContext, useEffect, useState } from 'react';
 import { addTodo } from './redux/actions';
 import ReduxContext from './contexts/ReduxContext';
 
-function App() {
+// Custom Hook
+// 상태 state에 getState()로 변화된 store의 데이터를 담아서(setState) 리턴.
+// 커스텀 훅 useReduxState()을 state에 할당해서 App()에서 보여주기
+function useReduxState() {
   const store = useContext(ReduxContext);  // useContext로  ReduxContext.Provider에 있는 store 부르기
   const [state, setState] =useState(store.getState());
+  
   useEffect(() => {
     const unsubscribe = store.subscribe(() => {
       setState(store.getState());
@@ -15,6 +19,12 @@ function App() {
     }
   }, [store])
 
+  return state;
+}
+
+function App() {
+  const state = useReduxState();
+
   return (
     <>
       {JSON.stringify(state)}
@@ -23,7 +33,7 @@ function App() {
   );
 
   function click() {
-    store.dispatch(addTodo('todo'));
+    // store.dispatch(addTodo('todo'));
   }
 }
 
