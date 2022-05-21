844cc3a35eb89e5ed845354c75b438eb0ad44df8
hellohyonee <hellohk07@gmail.com>
2022-05-21 22:47:17 +0900

    useContext로 store 불러오기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { addTodo } from './redux/actions';
+import ReduxContext from './contexts/ReduxContext';
 
-function App({ store }) {
+function App() {
+  const store = useContext(ReduxContext);  // useContext로  ReduxContext.Provider에 있는 store 부르기
   const [state, setState] =useState(store.getState());
   useEffect(() => {
     const unsubscribe = store.subscribe(() => {
