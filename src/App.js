9382ec58ea835bd817395e76ae5eb54769e4985f
hellohyonee <hellohk07@gmail.com>
2022-05-22 00:09:03 +0900

    TodoList 컴포넌트 구현 후 list 모양으로 todo 넣기

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { useContext, useEffect, useState } from 'react';
 import { addTodo } from './redux/actions';
 import useReduxState from './hooks/useReduxState';
 import useReduxDispatch from './hooks/useReduxDispatch';
-
+import TodoList from './components/TodoList';
 
 function App() {
-  const state = useReduxState();
+  // const state = useReduxState();  // --> TodoList 컴포넌트에서 useReduxState()로 state를 변경하므로 삭제
   const dispatch = useReduxDispatch();
 
   return (
     <>
-      {JSON.stringify(state)}
-      <button onClick={click}>추가</button>
+      {/* {JSON.stringify(state)} --> 삭제하고 TodoList 컴포넌트로 대체하기 */}
       <TodoList />
-      <TodoForm />
+      <button onClick={click}>추가</button>
+      {/* <TodoForm /> */}
     </>
   );
 
