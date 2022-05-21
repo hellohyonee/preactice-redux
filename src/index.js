1bf7c033dd009890142daba86ba63e5769ebad94
hellohyonee <hellohk07@gmail.com>
2022-05-21 22:14:12 +0900

    Context API로 store를 전체 컴포넌트에 내려주기

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import store from './redux/store';
+import ReduxContext from './contexts/ReduxContext';
 // import { addTodo, completeTodo, ShowComplete } from './redux/actions';
 
 // store.subscribe(() => {   // subscribe: store 변경사항을 구독 -> return 결과는 구독함수 제거
@@ -17,6 +18,9 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App store={store} />
+    <ReduxContext.Provider value={store}>
+      {/* ReduxContext.Provider에 props로 value를 보낼 수 있음. App에 보내던 store를 value에 넣기 */}
+      <App />  
+    </ReduxContext.Provider>
   </React.StrictMode>
 ); 
