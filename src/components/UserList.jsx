8e77fcdb6a8be48d66a11e0b7708898b2ebdf5ca
hellohyonee <hellohk07@gmail.com>
2022-05-23 17:20:11 +0900

    axios를 사용하여 github api users정보 받아오기-GET method (async/await)

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 export default function UserList({ users }) {
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        const res = await axios.get("https://api.github.com/users");
+      } catch (error) {}
+    }
+  }, []);
+
   if (users.length === 0) {
     return <p>현재 유저 정보 없음</p>;
   }
